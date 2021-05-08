0a4f256b59fd9987aa92d3ffb3589fd6cada764d
suvankars <suvankar.17@gmail.com>
2021-05-08 14:01:44 +0530

    adding dynamic fetching and loading npm modules

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,28 +1,54 @@
 import * as esbuild from "esbuild-wasm";
-
+import axios from "axios";
 export const unpkgPathPlugin = () => {
+
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log("onResolve", args);
-        return { path: args.path, namespace: "a" };
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } 
+        if (args.path.includes('./') || args.path.includes('../')){
+          return {
+            namespace: 'a',
+            path: new URL(args.path,'https://unpkg.com' + args.resolveDir + '/').href
+          }
+        }
+        
+        else
+          return {
+            path: `https://unpkg.com/${args.path}`,
+            namespace: "a",
+          };
       });
 
-      build.onLoad({ filter: /.*/ }, (args: any): esbuild.OnLoadResult | null => {
+      build.onLoad(
+        { filter: /.*/ },
+        async (args: any): Promise<any> => {
+          console.log("onLoad", args);
 
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: `
-              import message from 'tiny-test-pkg';
+          if (args.path === "index.js") {
+            return {
+              loader: "jsx",
+              contents: `
+              import React from 'react'
+              import ReactDOM from 'react-dom'
               console.log(message);
             `,
-          };  
-        } else return null
-      });
+            };
+          } else {
+            const { data, request } = await axios.get(args.path);
+            console.log(request);
+            return {
+              loader: "jsx",
+              contents: data,
+              resolveDir: new URL('./',request.responseURL  ).pathname
+            };
+          }
+        }
+      );
     },
   };
 };
