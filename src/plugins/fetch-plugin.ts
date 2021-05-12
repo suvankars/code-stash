f16a1be8670d3c1012f3d8f8752799fb7a0175ce
suvankars <suvankar.17@gmail.com>
2021-05-13 00:16:38 +0530

    adding css loader

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -23,22 +23,32 @@ export const fetchPlugin = (inputCode: string) => {
           } else {
             // IF file is already in cache then return from cache
 
-            const cacheData = await filecache.getItem<esbuild.OnLoadResult>(
-              args.path
-            );
-            console.log(cacheData);
-            if (cacheData) {
-              console.log("returning fromm cahce data");
-              return cacheData;
-            }
+            // const cacheData = await filecache.getItem<esbuild.OnLoadResult>(
+            //   args.path
+            // );
+            // console.log(cacheData);
+            // if (cacheData) {
+            //   console.log("returning fromm cahce data");
+            //   return cacheData;
+            // }
 
             // IF not then get it
             const { data, request } = await axios.get(args.path);
 
-            console.log(request);
+            const escaped = data.replace(/\n/g,'')
+                                .replace(/"/g,'\\"')
+                                .replace(/'/g, "\\'")
+            const fileType = args.path.match(/.css$/)? 'css' : 'jsx'
+            const contents = fileType === 'css' ?
+              `
+                const elm = document.createElement('style');
+                elm.innerText = '${escaped}'
+                document.head.appendChild(elm);
+              ` : data;
+
             const result: esbuild.OnLoadResult = {
-              loader: "jsx",
-              contents: data,
+              loader: 'jsx',
+              contents,
               resolveDir: new URL("./", request.responseURL).pathname,
             };
 
