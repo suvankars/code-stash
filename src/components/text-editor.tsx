08675830abebbb8a04e3952ce44bff45b7bae650
suvankars <suvankar.17@gmail.com>
2021-06-02 01:16:51 +0530

    adding onclick toggle between edit mode and preview mode

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,9 +1,43 @@
-import MDEditor from '@uiw/react-md-editor';
+import MDEditor from "@uiw/react-md-editor";
+import { useState, useEffect, useRef } from "react";
 
 const TextEditor: React.FC = () => {
-    return <div>
-        <MDEditor/>
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      console.log(event)
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source="# headder" />
     </div>
-}
+  );
+};
 
 export default TextEditor;
